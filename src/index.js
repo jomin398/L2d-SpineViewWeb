9ef491d4bd775c835ff6453bf3f38d116e2a364a
jomin398 <jomin398@gmail.com>
2021-12-21 07:48:27 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,81 +1,92 @@
 (doc => {
-    const client = {
-        width: null
-    }
-    window.onload = () => {
-        client.width = Number(doc.body.clientWidth);
-        console.log(client.width)
-        const app = new PIXI.Application({
-            width: 400, height: 400
-        });
-        const btnwarp = document.createElement('div');
-        btnwarp.className = 'btnwarp';
+  const client = {
+    width: null,
+    scale: 0.2 //default
+  }
+  window.onload = () => {
+    client.width = Number(doc.body.clientWidth);
+    const app = new PIXI.Application({
+      resizeTo: doc.querySelector('.cnvWrap')
+    });
+    const btnwarp = document.createElement('div');
+    btnwarp.className = 'btnwarp';
 
-        function pOnclick() {
-            console.log('onclick')
-            app.view.parentElement.onclick = null;
-            app.loader.add([
-                {
-                    // name: "demon",
-                    // url: "./data/Demon.json",
-                    // metadata: {
-                    //     spineAtlasSuffix: ".txt"
-                    // }
-                    name: "arona",
-                    url: "./asset/arona/arona_workpage.json",
-                    metadata: {
-                        spineAtlasSuffix: ".atlas"
-                    }
-                }
-            ])
-                .load(onAssetsLoaded);
+    function pOnclick() {
+      console.log('onclick')
+      app.view.parentElement.onclick = null;
+      app.loader.add([
+        {
+          // name: "demon",
+          // url: "./data/Demon.json",
+          // metadata: {
+          //     spineAtlasSuffix: ".txt"
+          // }
+          name: "arona",
+          url: "./asset/arona/arona_workpage.json",
+          metadata: {
+            spineAtlasSuffix: ".atlas"
+          }
+          }
+      ]).load(onAssetsLoaded);
 
-            app.stage.interactive = true;
-            //console.log(app)
-            function onAssetsLoaded(loader, res) {
-                // create a spine boy
-                const Modelkeys = Object.keys(res);
-                const model = new PIXI.spine.Spine(res[Modelkeys[0]].spineData);
-                
-                // set the position
-                model.x = app.screen.width/2; //app.screen.width;
-                model.y = app.screen.height; //app.screen.height;
+      app.stage.interactive = true;
+      //console.log(app)
+      client.scale = app.screen.width / doc.body.clientWidth;
+      if (client.scale >= 2) {
+        client.scale = Number(((app.screen.width /
+          doc.body.clientWidth) / 10).toFixed(2)) + 0.08;
+      } else {
+        client.scale = client.scale - 0.55;
+      }
+      console.log(client.scale)
 
-                model.scale.set(0.1);
+      function onAssetsLoaded(loader, res) {
+        // create a spine boy
+        const Modelkeys = Object.keys(res);
+        const model = new PIXI.spine.Spine(res[Modelkeys[0]].spineData);
 
-                app.stage.addChild(model);
-                console.log(model)
-                console.log(res[Modelkeys[0]].data.animations);
-                const singleAnimations = Object.keys(res[Modelkeys[0]].data.animations);
-                const loopAnimations = [];
-                var bool = Object.keys(app.loader.resources)[0] =='arona';
-                console.log('model is arona? :',bool)
-                if(bool){
-                    loopAnimations.push('Idle_00');
-                }
-                const allAnimations = [].concat(singleAnimations, loopAnimations);
+        // set the position
+        model.x = (app.screen.width / 2) //app.screen.width/2; //app.screen.width;
+        model.y = app.screen.height; //app.screen.height;
 
-                let lastAnimation = '';
+        model.scale.set(client.scale);
+        //model.scale.set(0.25);
 
-                // Press the screen to play a random animation
-                app.stage.on('pointerdown', () => {
-                    let animation = '';
-                    do {
-                        animation = allAnimations[Math.floor(Math.random() * allAnimations.length)];
-                    } while (animation === lastAnimation);
-
-                    model.state.setAnimation(0, animation, loopAnimations.includes(animation));
-
-                    lastAnimation = animation;
-                });
-            }
+        app.stage.addChild(model);
+        console.log(model)
+        console.log(res[Modelkeys[0]].data.animations);
+        let singleAnimations = Object.keys(res[Modelkeys[0]].data.animations);
+        const loopAnimations = [];
+        var bool = Object.keys(app.loader.resources)[0] == 'arona';
+        console.log('model is arona? :', bool)
+        if (bool) {
+          singleAnimations=singleAnimations.filter(a =>!['Dummy','Dev'].includes(a));
+          console.log(singleAnimations)
+          loopAnimations.push('Idle_00', 'Idle_02');
         }
+        const allAnimations = [].concat(singleAnimations, loopAnimations);
 
-        doc.querySelector('.cnvWrap').append(app.view, btnwarp);
-        st = doc.createElement('button');
-        st.innerText = 'start';
-        st.onclick = () => { pOnclick() };
-        btnwarp.append(st)
-    };
+        let lastAnimation = '';
+
+        // Press the screen to play a random animation
+        app.stage.on('pointerdown', () => {
+          let animation = '';
+          do {
+            animation = allAnimations[Math.floor(Math.random() * allAnimations.length)];
+          } while (animation === lastAnimation);
+          console.log(animation)
+          model.state.setAnimation(0, animation, loopAnimations.includes(animation));
+
+          lastAnimation = animation;
+        });
+      }
+    }
+
+    doc.querySelector('.cnvWrap').append(app.view, btnwarp);
+    st = doc.createElement('button');
+    st.innerText = 'start';
+    st.onclick = () => { pOnclick() };
+    btnwarp.append(st)
+  };
 
 })(document)
