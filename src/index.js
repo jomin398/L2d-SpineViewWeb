5aaab5027458684bc1bf75e42781935b09883004
jomin398 <jomin398@gmail.com>
2021-12-22 11:06:58 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+const setups = {
+  tempChrModelData: [
+    {
+      name: "arona",
+      url: "./asset/arona/arona_workpage.json",
+      metadata: {
+        spineAtlasSuffix: ".atlas"
+      }
+    }
+  ]
+}
 const client = {
+  doc: document,
+  app: null,
   width: null,
   //default
   scale: 0.2,
@@ -14,59 +27,56 @@ const client = {
       nAni: { n: '', o: null }
     }
   }
-}
+};
+
+const scaleCalc = (aw, cw) => {
+  let sclv = aw / cw;
+  let offset = 0;
+  console.log(sclv);
+  if (sclv >= 2) {
+    //mob vertical;
+    console.log(1)
+    offset = 0.05;
+  } else if (sclv >= 0.8) {
+    //mob hoz;
+    console.log(2)
+    offset = 0.15; //client.scale - 0.45;
+  } else {
+    console.log(3)
+    offset = 0.25;
+  }
+  sclv = Number(((aw / cw) / 10).toFixed(2)) + offset;
+  console.log(sclv);
+  return sclv;
+};
+function autoResize(model) {
+  //client.scale = app.screen.width / client.doc.body.clientWidth;
+  client.scale = scaleCalc(client.app.screen.width, client.doc.body.clientWidth);
+  if (model) {
+    console.log('orientation is changed')
+    model.scale.set(client.scale);
+  }
+};
+
 window.onload = () => {
-  const doc = document;
-  client.width = Number(doc.body.clientWidth);
-  const app = new PIXI.Application({
-    resizeTo: doc.querySelector('.cnvWrap')
+  const doc = client.doc;
+  client.width = Number(client.doc.body.clientWidth);
+  client.app = new PIXI.Application({
+    resizeTo: client.doc.querySelector('.cnvWrap')
   });
+
   const btnwarp = document.createElement('div');
   btnwarp.className = 'btnwarp';
-  function autoResize(model) {
-    client.scale = app.screen.width / doc.body.clientWidth;
-    console.log(client.scale)
-    if (client.scale >= 2) {
-      console.log(1)
-      client.scale = Number(((app.screen.width /
-        doc.body.clientWidth) / 10).toFixed(2));
-    } else if (client.scale >= 0.8) {
-      console.log(2)
-      client.scale = Number(((app.screen.width /
-        doc.body.clientWidth) / 10).toFixed(2)) + 0.15//client.scale - 0.45;
-    } else {
-      console.log(3)
-      client.scale = Number(((app.screen.width /
-        doc.body.clientWidth) / 10).toFixed(2)) + 0.24
-    }
-    console.log(client.scale)
-    if (model) {
-      console.log('orientation is changed')
-      model.scale.set(client.scale);
-    }
-  }
+
   function pOnclick() {
+    const app = client.app;
     console.log('onclick')
     app.view.parentElement.onclick = null;
-    app.loader.add([
-      {
-        // name: "demon",
-        // url: "./data/Demon.json",
-        // metadata: {
-        //     spineAtlasSuffix: ".txt"
-        // }
-        name: "arona",
-        url: "./asset/arona/arona_workpage.json",
-        metadata: {
-          spineAtlasSuffix: ".atlas"
-        }
-      }
-    ]).load(onAssetsLoaded);
+    app.loader.add(setups.tempChrModelData).load(onAssetsLoaded);
 
     app.stage.interactive = true;
     //console.log(app)
     autoResize();
-
 
     function onAssetsLoaded(loader, res) {
       // create a spine boy
@@ -86,7 +96,7 @@ window.onload = () => {
       //model.scale.set(0.25);
 
       app.stage.addChild(model);
-      console.log(model)
+      //console.log(model)
       console.log(res[Modelkeys[0]].data.animations);
 
 
@@ -94,7 +104,7 @@ window.onload = () => {
       console.log('model is arona? :', bool)
       if (bool) {
         //singleAnimations = singleAnimations.filter(a => !['Dummy', 'Dev'].includes(a));
-        console.log(client.model.now.sAni)
+        //console.log(client.model.now.sAni)
         loopAnimations.push('Idle_00', 'Idle_02');
       }
       client.model.now.aAni = [].concat(client.model.now.sAni, loopAnimations);
@@ -113,13 +123,13 @@ window.onload = () => {
       });
       window.addEventListener('orientationchange', () => autoResize(model), false);
       doc.querySelector('.cnvWrap button').innerText = 'reload';
-      doc.querySelector('.cnvWrap button').onclick = ()=>location.reload();
+      doc.querySelector('.cnvWrap button').onclick = () => location.reload();
     }
-  }
+  };
 
-  doc.querySelector('.cnvWrap').append(app.view, btnwarp);
-  st = doc.createElement('button');
+  client.doc.querySelector('.cnvWrap').append(client.app.view, btnwarp);
+  st = client.doc.createElement('button');
   st.innerText = 'start';
   st.onclick = () => { pOnclick() };
-  btnwarp.append(st)
+  btnwarp.append(st);
 };
